0f0559f096409bc5eb18652fa7a949b30391b750
pedroj0se <112190514+pedroj0se@users.noreply.github.com>
2023-07-17 09:21:28 -0500

    Update SearchItem.jsx

--- src/Components/searchItem/SearchItem.jsx
+++ src/Components/searchItem/SearchItem.jsx
@@ -5,7 +5,7 @@ const SearchItem = () => {
   return (
     <div className="searchItem">
       <img
-        src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1"
+        src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/06/1e/e3/50/jw-marriott-hotel-mumbai.jpg?w=1200&h=-1&s=1"
         alt=""
         className="siImg"
       />
